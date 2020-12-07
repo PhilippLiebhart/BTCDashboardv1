8ffb641ab5ba586babcbeca79a935783247bd482
Philipp Liebhart <philipp@philipp-liebhart.de>
2020-12-07 22:10:10 +0100

    cleanup

--- client/src/components/Dashboard/FinhubNewsItem.js
+++ client/src/components/Dashboard/FinhubNewsItem.js
@@ -1,8 +1,6 @@
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
-import "./FinhubNewsItem.css";
-
 const FinhubnewsItem = ({ id, headline, summary, url, time }) => {
   let unix_timestamp = time;
 
@@ -10,13 +8,11 @@ const FinhubnewsItem = ({ id, headline, summary, url, time }) => {
   let hours = date.getHours();
   let minutes = "0" + date.getMinutes();
   let seconds = "0" + date.getSeconds();
-
-  // Will display time in 10:30:23 format
   let formattedTime =
     hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
 
   return (
-    <>
+    <FinhubNewsItemWrapper>
       <main>
         <details closed="closed">
           <summary>
@@ -41,12 +37,53 @@ const FinhubnewsItem = ({ id, headline, summary, url, time }) => {
           </div>
         </details>
       </main>
-    </>
+    </FinhubNewsItemWrapper>
   );
 };
 
 const NewsItemDate = styled.span`
   font-size: 0.8rem;
+`;
+
+const FinhubNewsItemWrapper = styled.div`
+  summary {
+    outline: none;
+    cursor: pointer;
+    position: relative;
+    border-radius: 0.25rem;
+  }
+  details[open] summary ~ * {
+    animation: sweep 0.5s ease-in-out;
+  }
+  @keyframes sweep {
+    0% {
+      opacity: 0;
+      margin-top: -10px;
+    }
+    100% {
+      opacity: 1;
+      margin-top: 0px;
+    }
+  }
+  details > summary::before {
+    position: absolute;
+    content: "+";
+    right: 20px;
+  }
+  details[open] > summary::after {
+    position: absolute;
+    content: "-";
+    right: 20px;
+  }
+  details[open] > summary::before {
+    position: absolute;
+    content: "";
+    right: 20px;
+  }
+
+  details > summary::-webkit-details-marker {
+    display: none;
+  }
 `;
 
 FinhubnewsItem.propTypes = {};
